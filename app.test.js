186b04c9ed211acfce0350e2c5350260e2f3d9a6
DanielAlticeLabs <95239516+DanielAlticeLabs@users.noreply.github.com>
2021-11-30 16:12:23 +0000

    changes

--- app.test.js
+++ app.test.js
@@ -2,6 +2,6 @@ test('Should Pass', () => {
     expect(true).toBe(true);
 });
 
-test('Should Fail', () => {
-    expect(false).toBe(true);
+test('Should (Not) Fail', () => {
+    expect(false).toBe(false);
 });
